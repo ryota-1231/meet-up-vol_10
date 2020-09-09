ca2fea2b6d5459a15474b7630d33ae750cb783fd
miracleave-ltd <info@miracleave.co.jp>
2020-09-09 21:29:51 +0900

    fix

--- my-laravel-app/tests/Feature/BookTest.php
+++ my-laravel-app/tests/Feature/BookTest.php
@@ -38,7 +38,7 @@ class BookTest extends TestCase
     {
         $response = $this->get('/books/show');
 
-        $response->assertStatus(500);
+        $response->assertStatus(200);
     }
 
     /**
